a504e1a61e6d531d675c100dce6912b6fce13fed
ra2085 <ra.2085@gmail.com>
2018-09-06 10:05:14 -0500

    Update JS-Compare-Hash.js
    Expect false always.

--- apiproxy/resources/jsc/JS-Compare-Hash.js
+++ apiproxy/resources/jsc/JS-Compare-Hash.js
@@ -5,6 +5,7 @@
  var upperTimeLimit = timestamp + 301;
  var lowerTimeLimit = timestamp - 300;
  var _md5 = crypto.getMD5();
+ context.setVariable("flow.signature.valid", false);
  for(var i = lowerTimeLimit; i < upperTimeLimit; i++){
      _md5.update(context.getVariable("verifyapikey.VK-Verfy-Key.client_id"));
      _md5.update(context.getVariable("verifyapikey.VK-Verfy-Key.client_secret"));
@@ -13,7 +14,5 @@
      if(context.getVariable("request.queryparam.sig") == toCompare){
          context.setVariable("flow.signature.valid", true);
          break;
-     } else {
-         context.setVariable("flow.signature.valid", false);
      }
  }
